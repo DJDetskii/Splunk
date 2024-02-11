ec2ca79650d3efc5c8539746e04899b4ea4a76f2
DJDetskii <112144756+DJDetskii@users.noreply.github.com>
2024-02-10 21:45:13 -0600

    Update splunkADDNS.ps1

--- splunkADDNS.ps1
+++ splunkADDNS.ps1
@@ -1,35 +1,17 @@
-# Variables
-$splunkInstaller = "C:\Desktop\splunkforwarder-9.1.1-64e843ea36b1-x64-release-airgap.msi"
-$inputsConfPath = "C:\Program Files\SplunkUniversalForwarder\etc\system\local"
-$outputsConfPath = "C:\Program Files\SplunkUniversalForwarder\etc\system\local\outputs.conf"
+# Monitor Active Directory Logs
+[monitor://C:\Windows\debug\Ntds\*.log]
+disabled = false
+sourcetype = WinEventLog:Security
+index = main
 
-# Install Splunk Universal Forwarder
-Start-Process -FilePath msiexec -ArgumentList "/i `"$splunkInstaller`" /quiet" -Wait
-
-# Configure inputs.conf
-@"
+# Monitor DNS Server Logs
 [monitor://C:\Windows\System32\winevt\Logs\Microsoft-Windows-DNSServer%4Analytical.etl]
-sourcetype = dns_log
+disabled = false
+sourcetype = WinEventLog:Microsoft-Windows-DNSServer-Analytical
 index = main
 
+# Monitor DHCP Server Logs
 [monitor://C:\Windows\System32\dhcp\DhcpSrvLog-*.log]
-sourcetype = dhcp_log
+disabled = false
+sourcetype = WinEventLog:Microsoft-Windows-DHCP-Server
 index = main
-
-[monitor://C:\Windows\System32\winevt\Logs\Directory Service]
-sourcetype = ad_log
-index = main
-"@ | Set-Content -Path $inputsConfPath -Force
-
-# Configure outputs.conf
-@"
-[tcpout]
-defaultGroup = WinADdns
-
-[tcpout:WinADdns]
-server = 172.20.241.20:9997
-"@ | Set-Content -Path $outputsConfPath -Force
-
-# Start Splunk Universal Forwarder service
-Start-Service -Name SplunkForwarder
-
